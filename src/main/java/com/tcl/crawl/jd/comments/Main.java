8b45de26554d118edc41504e027042749a101bda
tangbiao <tangbiao@live.cn>
2017-03-21 14:03:54 +0800

    修改main，添加url的方法

--- src/main/java/com/tcl/crawl/jd/comments/Main.java
+++ src/main/java/com/tcl/crawl/jd/comments/Main.java
@@ -15,16 +15,23 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String url1 = "https://list.jd.com/list.html?cat=737,794,798&ev=5305_7188%40exbrand_2505&sort=sort_totalsales15_desc&trans=1&JL=3_%E5%93%81%E7%89%8C_TCL#J_crumbsBar";
-        String url2 = "https://list.jd.com/list.html?cat=737,794,798&ev=5305_7188%40exbrand_2505&page=2&sort=sort_totalsales15_desc&trans=1&JL=6_0_0#J_main";
-        // String url1 = "https://item.jd.com/4261888.html";
-        // String url2 = "https://item.jd.com/10627656945.html";
+        List<String> urls = new ArrayList<String>();
+        //urls.add("https://club.jd.com/repay/2634567_31a199d4-656c-4413-ac97-8378741536ed_1.html");
+        urls.add("https://list.jd.com/list.html?cat=737,794,798&ev=5305_7188%40exbrand_2505&sort=sort_totalsales15_desc&trans=1&JL=3_%E5%93%81%E7%89%8C_TCL#J_crumbsBar");
+        urls.add("https://list.jd.com/list.html?cat=737,794,798&ev=5305_7188%40exbrand_2505&page=2&sort=sort_totalsales15_desc&trans=1&JL=6_0_0#J_main");
+//        urls.add("https://item.jd.com/4261888.html");
+//        urls.add("https://item.jd.com/10627656945.html");
 
         List<Pipeline> pipelines = new ArrayList<Pipeline>();
         pipelines.add(new ConsolePipeline());
         pipelines.add(new JdbcPipeline());
 
-        Spider.create(new JDCommentsCrawl()).setPipelines(pipelines).addUrl(url1).addUrl(url2).thread(10).run();
+        Spider spider = Spider.create(new JDCommentsCrawl()).setPipelines(pipelines);
+        for (String url :
+                urls) {
+            spider.addUrl(url);
+        }
+        spider.thread(10).run();
     }
 
 }
